9946ce66468be15c8cecaaeb10b8a9725413888d
Patemole <77934579+Patemole@users.noreply.github.com>
2025-03-11 11:51:03 +0100

    bug

--- src/routes/SignIn_Sign_up_Onboarding/signUp.js
+++ src/routes/SignIn_Sign_up_Onboarding/signUp.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { createUserWithEmailAndPassword, OAuthProvider, signInWithPopup} from 'firebase/auth';
 import { auth, db } from '../../auth/firebase';
