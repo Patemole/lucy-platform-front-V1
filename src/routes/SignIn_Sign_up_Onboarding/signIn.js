e6eb11f543206cc58c256fa5ef059779415db9fe
Lucy-CTO <lucy-cto@MacBook-Pro-de-Lucy-CTO.local>
2025-03-21 12:24:29 -0400

    Default user issue

--- src/routes/SignIn_Sign_up_Onboarding/signIn.js
+++ src/routes/SignIn_Sign_up_Onboarding/signIn.js
@@ -184,8 +184,8 @@ const SignIn = ({ handleToggleThemeMode }) => {
         //login(userData);
         login({
           id: userData.uid, // Assure la cohérence avec le SSO
-          //name: userData.name || "",
-          name: user.displayName || "",
+          name: userData.name || "",
+          //name: user.displayName || "",
           email: userData.email,
           university: userData.university,
           onboardingComplete: userData.onboardingComplete,
