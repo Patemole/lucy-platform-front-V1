2973f8e793f8a408b1600579857ab179acf95117
Lucy-CTO <lucy-cto@lev-01109.apn.wlan.private.upenn.edu>
2025-03-28 09:50:26 -0400

    Modification of the number of day left

--- src/components/main_components/LandingPageImprove.tsx
+++ src/components/main_components/LandingPageImprove.tsx
@@ -83,6 +83,16 @@ const LandingPage: React.FC<LandingPageProps> = ({ onSend, onPrivacyChange, upda
     "Default": "#7F8C8D" // Gris
   };
 
+  //Calculate the number of day before 31 march
+  const daysLeftUntilMarch31 = () => {
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear(), 2, 31); // Mois indexé à 0 (mars = 2)
+    const differenceInTime = targetDate.getTime() - today.getTime();
+    const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+    
+    return differenceInDays >= 0 ? differenceInDays : 0;
+  };
+
   // Fonction pour envoyer le message
   const handleSend = () => {
     const message = inputValue.trim();
@@ -561,6 +571,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onSend, onPrivacyChange, upda
         />
 
         {/* Affichage conditionnel pour Holy Family seulement */}
+      {/* Affichage conditionnel pour Holy Family seulement */}
       {subdomain === 'holyfamily' && (
         <Box
           sx={{
@@ -583,10 +594,10 @@ const LandingPage: React.FC<LandingPageProps> = ({ onSend, onPrivacyChange, upda
               fontWeight: '500'
             }}
           >
-            Lucy Game: 7 days left to be the top user and win 2 plane tickets to Miami 🏖️
+            Lucy Game: {daysLeftUntilMarch31()} days left to be the top user and win round trip for 2 to Miami 🏖️
           </Typography>
         </Box>
-        )}
+      )}
 
         {/* Afficher les inspirations sous le placeholder sur petit écran */}
         {isSmallScreen && (
