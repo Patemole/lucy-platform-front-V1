f81edcf303b6104cc131f3d211f3492b0cf64859
Lucy-CTO <lucy-cto@MacBook-Pro-de-Lucy-CTO.local>
2025-04-23 10:32:44 -0400

    Can t click on profile when onboarding

--- src/routes/chat/hooks/useOnboarding.ts
+++ src/routes/chat/hooks/useOnboarding.ts
@@ -58,7 +58,8 @@ export const useOnboarding = ({
 
   // --- Constantes ---
   const onboardingMessages = [
-    { question: "What is your current school?", metadata: "SCHOOL" },       // 0
+    //{ question: "What is your current school?", metadata: "SCHOOL" },       // 0
+    { question: "quick q’s for you—this’ll help me grasp the real you so i can answer your question properly. Please answer as much as you can so i can help you the right way:", metadata: "SCHOOL" },   
     { question: "What year are you in?", metadata: "YEAR" },         // 1
     { question: "What's your Insta?", metadata: "INSTAGRAM" },    // 2
     { question: "What is you linkedin URL?", metadata: "LINKEDIN" }, // 3
--- src/routes/chat/onboardingLucyQuestions.tsx
+++ src/routes/chat/onboardingLucyQuestions.tsx
@@ -250,7 +250,8 @@ const OnboardingLucyQuestions: React.FC = ()=> {
    */
 
   const onboardingMessages = [
-    { question: "What is your current school?", metadata: "SCHOOL" },
+    //{ question: "What is your current school?", metadata: "SCHOOL" },       // 0
+    { question: "quick q’s for you—this’ll help me grasp the real you so i can answer your question properly. Please answer as much as you can so i can help you the right way:", metadata: "SCHOOL" },   
     { question: "What year are you in?", metadata: "YEAR" },
     { question: "This is a test, are you Mathieu?", metadata: "TEST" },
     { question: "What's your Insta?", metadata: "INSTAGRAM" },
